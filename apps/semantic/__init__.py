6a6d3a382243a8d22fc4e884be287cb55ab4faf6
Christian Bick <christian.bick@bitsuppliers.com>
2024-09-30 16:28:58 +0200

    Update poc

--- apps/semantic/__init__.py
+++ apps/semantic/__init__.py
@@ -1,6 +1,5 @@
 from owlready2 import *
 
-onto = get_ontology("./../core-ontology.rdf").load()
-onto.load()
 
+onto = get_ontology("./../core-ontology.rdf").load()
 onto.base_iri = "http://edugraph.io/edu"
--- apps/semantic/__main__.py
+++ apps/semantic/__main__.py
@@ -1,3 +1,6 @@
 from semantic import *
 
-print(onto.Arithmetic.INDIRECT_partOf)
+#owlready2.JAVA_EXE = "C:\\Users\\silen\\Protege-5.6.4\\jre\\bin\\java.exe"
+#sync_reasoner()
+
+print(onto.Arithmetic.INDIRECT_hasPart)
