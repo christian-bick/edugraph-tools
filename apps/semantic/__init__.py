fff358317e13d3c581731cacd1b8c96011723da2
Christian Bick <christian.bick@bitsuppliers.com>
2024-09-30 16:46:17 +0200

    Add .env file support

--- apps/semantic/__init__.py
+++ apps/semantic/__init__.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
 from owlready2 import *
 
+load_dotenv()
+
+owlready2.JAVA_EXE = os.getenv("JAVA_RUNTIME")
+sync_reasoner()
 
 onto = get_ontology("./../core-ontology.rdf").load()
 onto.base_iri = "http://edugraph.io/edu"
