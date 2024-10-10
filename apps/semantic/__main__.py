6f2f60c52e6fcae440e5b81d1263a8f8f154f83b
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-10 19:38:55 +0200

    Fix sporadic returning of phrased out result

--- apps/semantic/__main__.py
+++ apps/semantic/__main__.py
@@ -91,20 +91,22 @@ def find_best_match(description, context, descriptor_type):
                 {1}
 
                 Find the most accurate {2} that matches the describe learning material, 
-                only using terminology that was defined in the taxonomy.
+                * only using terminology that was defined in the taxonomy
+                * preferring low-level {2}s over high-level {2}s 
 
-                Return the best match without chapter number
-            """.format(context, description, descriptor_type))
+                Only return the best match without chapter number
+            """.format(context, description, descriptor_type_name))
 
     result = model.generate_content(
         prompt, generation_config=gemini.types.GenerationConfig(
             candidate_count=1,
             max_output_tokens=250,
-            response_mime_type="text/plain",
+            response_mime_type="application/json",
             temperature=0,
-            response_schema=str
+            response_schema=list[str]
         ))
-    return node_of_value(result.text.strip())
+    result_list = json.loads(result.text)
+    return [node_of_value(value) for value in result_list]
 
 def find_matches(description, context, descriptor_type):
     model = gemini.GenerativeModel(
@@ -126,10 +128,10 @@ def find_matches(description, context, descriptor_type):
                 Find the applicable {2}s for the described learning material,
                 * only using terminology that was defined in the taxonomy
                 * only using the conditions as defined in the taxonomy
-                * only when you are certain about your judgement
+                * preferring low-level {2}s over high-level {2}s 
                 
                 Return matched results without their chapter numbers
-            """.format(context, description, descriptor_type))
+            """.format(context, description, descriptor_type_name))
 
     result = model.generate_content(
         prompt, generation_config=gemini.types.GenerationConfig(
