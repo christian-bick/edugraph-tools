e666abb757d830200915597ba43508dacb6d0ef7
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-12 01:10:09 +0200

    Eliminate an extra description call.

--- apps/semantic/gemini_classifier.py
+++ apps/semantic/gemini_classifier.py
@@ -11,37 +11,34 @@ class GeminiClassifier:
         self.context_builder = ContextBuilder()
         self.ClassifierStrategy = ClassifierStrategy
 
-    def classify_area(self, description, classifier):
+    def classify_area(self, classifier):
         descriptor_type = self.onto.Area
         nodes = [ self.onto.Mathematics ]
         context = self.context_builder.build_area_context(nodes)
-        area = classifier.find_best_match(description, context, descriptor_type)
+        area = classifier.find_best_match(context, descriptor_type)
         return area
 
-    def classify_ability(self, description, classifier):
+    def classify_ability(self, classifier):
         descriptor_type = self.onto.Ability
         nodes = [self.onto.AnalyticalCapability]
         context = self.context_builder.build_ability_context(nodes)
-        matched_areas = classifier.find_matches(description, context, descriptor_type)
+        matched_areas = classifier.find_matches(context, descriptor_type)
         return matched_areas
 
-    def classify_scope(self, description, classifier):
+    def classify_scope(self, classifier):
         descriptor_type = self.onto.Scope
         nodes = [self.onto.RepresentationalScope, self.onto.AbstractionScope, self.onto.MeasurementScope]
         context = self.context_builder.build_scope_context(nodes)
-        matched_scopes = classifier.find_matches(description, context, descriptor_type)
+        matched_scopes = classifier.find_matches(context, descriptor_type)
         return matched_scopes
 
     def classify_content(self, file):
         gemini_file = GeminiFileStorage.upload(file)
         classifier = self.ClassifierStrategy(gemini_file)
-        description = classifier.describe_content()
-        return self.classify_text(description, classifier)
-
-    def classify_text(self, text, classifier):
         classification = {
-            "area": self.classify_area(text, classifier),
-            "abilities": self.classify_ability(text, classifier),
-            "scopes": self.classify_scope(text, classifier)
+            "area": self.classify_area(classifier),
+            "abilities": self.classify_ability(classifier),
+            "scopes": self.classify_scope(classifier)
         }
         return classification
+
--- apps/semantic/gemini_prompt_strategy.py
+++ apps/semantic/gemini_prompt_strategy.py
@@ -6,56 +6,42 @@ from semantic.ontology_helper import OntologyHelper
 
 gemini.configure(api_key=os.environ["API_KEY_GEMINI"])
 
+description_template = """
+Type of learning material:
+
+<Description of its physical or digital nature in 1 sentence>
+
+Content description:
+
+<For text files, a summary of the text, for pictures, a description of the picture, using up to 
+500 words>
+
+Used forms of representation:
+
+<Description of different forms of representation in up to 5 bullet points>
+
+Field and area of learning:
+
+<Describe the field and area of learning in 1-2 sentences>
+
+<Describe the main learning abilities used when interacting with the material in 1-3 sentence>
+
+<Describe the intention behind the learning material>
+
+Other observations:
+
+<Optionally add other significant observations about the learning material>
+"""
+
 class GeminiPromptStrategy:
 
     def __init__(self, gemini_file):
         self.gemini_file = gemini_file
 
-    def describe_content(self):
+    def find_best_match(self, taxonomy, descriptor_type):
         model = gemini.GenerativeModel(
             model_name="gemini-1.5-flash",
-            system_instruction="You are presented with learning material that you shall describe as accurately as possible."
-        )
-
-        prompt = """Describe the provided file using the following pattern:
-    
-            Type of learning material:
-    
-            <Description of its physical or digital nature in 1 sentence>
-    
-            Content description:
-    
-            <For text files, a summary of the text, for pictures, a description of the picture, using up to 
-            500 words>
-    
-            Used forms of representation:
-    
-            <Description of different forms of representation in up to 5 bullet points>
-    
-            Field and area of learning:
-    
-            <Describe the field and area of learning in 1-2 sentences>
-    
-            <Describe the main learning abilities used when interacting with the material in 1-3 sentence>
-    
-            <Describe the intention behind the learning material>
-    
-            Other observations:
-    
-            <Optionally add other significant observations about the learning material>
-        """
-
-        result = model.generate_content(
-            [self.gemini_file, prompt], generation_config=gemini.types.GenerationConfig(
-                candidate_count=1,
-                max_output_tokens=2000,
-            ))
-        return result.text
-
-    def find_best_match(self, description, context, descriptor_type):
-        model = gemini.GenerativeModel(
-            model_name="gemini-1.5-flash",
-            system_instruction="You are presented with descriptions of learning material that you shall classify."
+            system_instruction="You are presented with learning material that you shall classify."
         )
 
         descriptor_type_name = OntologyHelper.id_as_name(descriptor_type.name)
@@ -64,20 +50,21 @@ class GeminiPromptStrategy:
                     Consider the following taxonomy:
     
                     {0}
+                    
+                    Describe the provided file using the following pattern:
+                    
+                    {2}
     
-                    Consider the following description of learning material
-    
-                    {1}
-    
-                    Find the most accurate {2} that matches the describe learning material, 
+                    Then find the most accurate {1} that matches the describe learning material, 
                     * only using terminology that was defined in the taxonomy
-                    * preferring low-level {2}s over high-level {2}s 
+                    * preferring low-level {1}s over high-level {1}s 
     
-                    Only return the best match without chapter number
-                """.format(context, description, descriptor_type_name))
+                    Do not return the description.
+                    Return the single best match without chapter number.
+                """.format(taxonomy, descriptor_type_name, description_template))
 
         result = model.generate_content(
-            prompt, generation_config=gemini.types.GenerationConfig(
+            [self.gemini_file, prompt], generation_config=gemini.types.GenerationConfig(
                 candidate_count=1,
                 max_output_tokens=250,
                 response_mime_type="application/json",
@@ -87,10 +74,10 @@ class GeminiPromptStrategy:
         result_list = json.loads(result.text)
         return result_list
 
-    def find_matches(self, description, context, descriptor_type):
+    def find_matches(self, taxonomy, descriptor_type):
         model = gemini.GenerativeModel(
             model_name="gemini-1.5-flash",
-            system_instruction="You are presented with descriptions of learning material that you shall classify."
+            system_instruction="You are presented with learning material that you shall classify."
         )
 
         descriptor_type_name = OntologyHelper.id_as_name(descriptor_type.name)
@@ -99,21 +86,22 @@ class GeminiPromptStrategy:
                     Consider the following taxonomy:
     
                     {0}
+                    
+                    Describe the provided file using the following pattern:
+                    
+                    {2}
     
-                    Consider the following description of learning material
-    
-                    {1}
-    
-                    Find the applicable {2}s for the described learning material,
+                    Then find the applicable {1}s for the described learning material,
                     * only using terminology that was defined in the taxonomy
                     * only using the conditions as defined in the taxonomy
-                    * preferring low-level {2}s over high-level {2}s 
+                    * preferring low-level {1}s over high-level {1}s 
     
-                    Return matched results without their chapter numbers
-                """.format(context, description, descriptor_type_name))
+                    Do not return the description.
+                    Return matched results without their chapter numbers.
+                """.format(taxonomy, descriptor_type_name, description_template))
 
         result = model.generate_content(
-            prompt, generation_config=gemini.types.GenerationConfig(
+            [self.gemini_file, prompt], generation_config=gemini.types.GenerationConfig(
                 candidate_count=1,
                 max_output_tokens=250,
                 response_mime_type="application/json",
