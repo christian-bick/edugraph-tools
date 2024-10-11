d72f398029ec644f5b0e529516aebee6e06f2ee2
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-11 15:20:32 +0200

    Add first UI elements

--- web/main.js
+++ web/main.js
@@ -1,1 +1,12 @@
 import "./style.scss";
+
+const switchView = (oldId, newId) => {
+    document.getElementById(newId).style.display = "flex";
+    document.getElementById(oldId).style.display = "none";
+}
+
+const init = () => {
+    setTimeout(() => switchView('view-init', 'view-credentials'), 1000)
+}
+
+document.addEventListener("DOMContentLoaded", init);
