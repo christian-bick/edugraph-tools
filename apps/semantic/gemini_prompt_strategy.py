277f6fcb1105bf72dc1d53ab925de850732c5dd3
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-12 20:36:14 +0200

    Refine prompts

--- apps/semantic/gemini_prompt_strategy.py
+++ apps/semantic/gemini_prompt_strategy.py
@@ -33,6 +33,64 @@ Other observations:
 <Optionally add other significant observations about the learning material>
 """
 
+system_instruction = """
+You are presented with learning material that you shall classify using a given taxonomy.
+
+The taxonomy will always consist of two parts, an outline and definition section. 
+
+Here is an example of a taxonomy for gastronomic terms:
+
+---
+Taxonomy of Gastronomy
+
+A) Outline
+
+1 Drinks
+1.1 Non-alcoholic Drinks
+1.2 Alcoholic Drinks
+1.2.1 Beer
+1.2.2 Wine
+2 Food
+2.1 Italian
+2.2 French
+
+B) Definitions
+
+1 Drinks
+
+Beverages.
+
+1.1 Non-alcoholic
+
+Beverages without alcohol.
+
+1.2.1 Beer 
+
+A malty alcoholic drink
+
+1.2.2 Wine
+
+An alcoholic drink made of grapes
+
+2 Food
+
+2.1 Italian
+
+Food typically prepared in Italy
+
+2.2 French
+
+Food typically prepared in France
+---
+
+The hierarchy of terms in the outline expresses a "part of" relationship. In the example, beer is part of alcoholic 
+beverages and alcoholic beverages are part of drinks. Use this hierarchy to find the most specific classification 
+matches.
+
+Sometimes, terms in the outline are defined in definition section. When available, use these definitions when 
+determining classification matches.
+"""
+
 class GeminiPromptStrategy:
 
     def __init__(self, gemini_file):
@@ -41,7 +99,7 @@ class GeminiPromptStrategy:
     def find_best_match(self, taxonomy, descriptor_type):
         model = gemini.GenerativeModel(
             model_name="gemini-1.5-flash",
-            system_instruction="You are presented with learning material that you shall classify."
+            system_instruction=system_instruction
         )
 
         descriptor_type_name = OntologyUtil.entity_native_name(descriptor_type)
@@ -56,11 +114,10 @@ class GeminiPromptStrategy:
                     {2}
     
                     Then find the most accurate {1} that matches the describe learning material, 
-                    * only using terminology that was defined in the taxonomy
-                    * preferring low-level {1}s over high-level {1}s 
+                    only using {1}s that were defined in the taxonomy.
     
                     Do not return the description.
-                    Return the single best match without chapter number.
+                    Return the single best match without its outline number.
                 """.format(taxonomy, descriptor_type_name, description_template))
 
         result = model.generate_content(
@@ -77,7 +134,7 @@ class GeminiPromptStrategy:
     def find_matches(self, taxonomy, descriptor_type):
         model = gemini.GenerativeModel(
             model_name="gemini-1.5-flash",
-            system_instruction="You are presented with learning material that you shall classify."
+            system_instruction=system_instruction
         )
 
         descriptor_type_name = OntologyUtil.entity_native_name(descriptor_type)
@@ -91,13 +148,11 @@ class GeminiPromptStrategy:
                     
                     {2}
     
-                    Then find the applicable {1}s for the described learning material,
-                    * only using terminology that was defined in the taxonomy
-                    * only using the conditions as defined in the taxonomy
-                    * preferring low-level {1}s over high-level {1}s 
+                    Then find accurately matching {1}s for the described learning material,
+                    only using {1}s that were defined in the taxonomy.
     
                     Do not return the description.
-                    Return matched results without their chapter numbers.
+                    Return the best matches without their outline number.
                 """.format(taxonomy, descriptor_type_name, description_template))
 
         result = model.generate_content(
--- apps/semantic/gimini_context_builder.py
+++ apps/semantic/gimini_context_builder.py
@@ -2,73 +2,95 @@ from functools import reduce
 
 from semantic.ontology_util import OntologyUtil
 
+taxonomy_header_template = "Taxonomy of {0}\n\n"
+outline_header_template = "\nA) {0} Outline\n\n"
+definition_header_template = "\nB) {0} Definitions\n\n"
+
 class ContextBuilder:
 
     def __init__(self):
         self.context = None
 
+    def add_taxonomy_header(self, name):
+        self.context += "Taxonomy of {0}\n\n".format(name)
+
+    def add_outline_header(self, name):
+        self.context += "\n\nA) {0} Outline\n\n".format(name)
+
+    def add_definition_header(self, name):
+        self.context += "\n\nB) {0} Definitions\n\n".format(name)
+
     def build_area_context(self, nodes):
-        hierarchy = [1]
-        self.context = "Taxonomy of Areas\n\n"
-        self.expand_index_context_rec(hierarchy, nodes)
-        self.expand_definition_context_rec(hierarchy, nodes)
+        name = "Area"
+        self.context = ""
+        self.add_taxonomy_header(name)
+        self.add_outline_header(name)
+        self.add_outline_context_rec([1], nodes)
+        self.add_definition_header(name)
+        self.add_definition_context_rec([1], nodes)
         return self.context
 
     def build_ability_context(self, nodes):
-        hierarchy = [1]
-        self.context = "Taxonomy of Abilities\n\n"
-        self.expand_index_context_rec(hierarchy, nodes)
-        self.expand_definition_context_rec(hierarchy, nodes)
+        name = "Ability"
+        self.context = ""
+        self.add_taxonomy_header(name)
+        self.add_outline_header(name)
+        self.add_outline_context_rec([1], nodes)
+        self.add_definition_header(name)
+        self.add_definition_context_rec([1], nodes)
         return self.context
 
     def build_scope_context(self, nodes):
-        hierarchy = [1]
-        self.context = "Taxonomy of Scopes\n\n"
-        self.expand_index_context_rec(hierarchy, nodes)
-        self.expand_definition_context_rec(hierarchy, nodes)
+        name = "Scope"
+        self.context = ""
+        self.add_taxonomy_header(name)
+        self.add_outline_header(name)
+        self.add_outline_context_rec([1], nodes)
+        self.add_definition_header(name)
+        self.add_definition_context_rec([1], nodes)
         return self.context
 
-    def expand_definition_context(self, hierarchy, node):
+    def add_definition_context(self, hierarchy, node):
 
         definition = node.isDefinedBy
 
         if isinstance(definition, list) and len(definition) > 0:
-            self.expand_index_context(hierarchy, node)
+            self.add_index_context(hierarchy, node)
             self.context += "\n{0}\n\n".format(definition[0])
 
-    def expand_definition_context_rec(self, hierarchy, nodes):
+    def add_definition_context_rec(self, hierarchy, nodes):
         current_index = len(hierarchy)-1
         current_counter = 1
 
         for node in nodes:
             hierarchy[current_index] = current_counter
-            self.expand_definition_context(hierarchy, node)
+            self.add_definition_context(hierarchy, node)
 
             if not OntologyUtil.is_leaf_entity(node):
                 descriptor_parts = node.INDIRECT_hasPart
                 new_hierarchy = hierarchy + [1]
-                self.expand_definition_context_rec(new_hierarchy, descriptor_parts)
+                self.add_definition_context_rec(new_hierarchy, descriptor_parts)
 
             current_counter = current_counter + 1
 
-    def expand_index_context(self, hierarchy, node):
+    def add_index_context(self, hierarchy, node):
         hierarchy_as_string = (
             ContextBuilder.hierarchy_to_string(hierarchy) + ' ' + OntologyUtil.entity_native_name(node) + '\n'
         )
         self.context += hierarchy_as_string
 
-    def expand_index_context_rec(self, hierarchy, nodes):
+    def add_outline_context_rec(self, hierarchy, nodes):
         current_index = len(hierarchy)-1
         current_counter = 1
 
         for node in nodes:
             hierarchy[current_index] = current_counter
-            self.expand_index_context(hierarchy, node)
+            self.add_index_context(hierarchy, node)
 
             if not OntologyUtil.is_leaf_entity(node):
                 descriptor_parts = node.INDIRECT_hasPart
                 new_hierarchy = hierarchy + [1]
-                self.expand_index_context_rec(new_hierarchy, descriptor_parts)
+                self.add_outline_context_rec(new_hierarchy, descriptor_parts)
 
             current_counter = current_counter + 1
 
