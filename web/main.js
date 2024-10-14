85255f2b8fffa505cc4b6860c4c2a2c9462ef12b
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-14 12:43:07 +0200

    Refine upload

--- web/main.js
+++ web/main.js
@@ -1,17 +1,107 @@
 import "./style.scss";
-import {Dropzone} from "dropzone";
 
 const UPLOAD_URL = "/api/classify"
 
-const switchView = (oldId, newId) => {
-    document.getElementById(newId).style.display = "flex";
-    document.getElementById(oldId).style.display = "none";
+const switchView = (oldEl, newEl) => {
+    newEl.style.display = "flex";
+    oldEl.style.display = "none";
 }
 
 const init = () => {
-    setTimeout(() => switchView('view-init', 'view-classification'), 1000)
-    const dropzone = new Dropzone("#upload-dropzone", { url: UPLOAD_URL });
+    const viewInit = document.getElementById('view-init')
+    const viewClassification = document.getElementById('view-init')
+    setTimeout(() => switchView(viewInit, viewClassification), 1000)
+    initFileUpload()
 
+}
+
+const initFileUpload = () => {
+    const viewUpload = document.getElementById('view-upload');
+    const viewProgress = document.getElementById('view-progress');
+    const viewResult = document.getElementById('view-result');
+
+    const uploadDropzone = document.getElementById('upload-dropzone');
+    const uploadInput = document.getElementById('upload-input');
+
+    // Prevent default drag behaviors
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        uploadDropzone.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+    }
+
+    // Highlight drop area when item is dragged over it
+    ['dragenter', 'dragover'].forEach(eventName => {
+        uploadDropzone.addEventListener(eventName, highlight, false);
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        uploadDropzone.addEventListener(eventName, unhighlight,
+            false);
+    });
+
+    function highlight(e) {
+        uploadDropzone.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        uploadDropzone.classList.remove('highlight');
+    }
+
+    // Handle dropped files
+    uploadDropzone.addEventListener('drop', handleDrop, false);
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer;
+
+        let files = dt.files;
+
+        handleFiles(files);
+    }
+
+    // Handle files from input element
+    uploadInput.addEventListener('change', handleFiles, false);
+
+    function handleFiles(files) {
+        uploadFile(files[0]);
+    }
+
+    function previewFile(file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            viewResult.innerHTML = `<img class="file-preview" src="${e.target.result}" alt="${file.name}"/>`;
+            switchView(viewProgress, viewResult)
+        }
+        reader.readAsDataURL(file);
+    }
+
+    function uploadFile(file) {
+        let url = UPLOAD_URL;
+        let formData = new FormData();
+        formData.append('file', file);
+
+        switchView(viewUpload, viewProgress)
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                previewFile(file)
+                console.log(response.json())
+            })
+
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 }
 
 document.addEventListener("DOMContentLoaded", init);
