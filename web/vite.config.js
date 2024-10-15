a2bb5ed8d804a98706bbf51d006434560ec195dd
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-16 00:31:16 +0200

    Enable dockerized development

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "vite";
 
 export default defineConfig({
     server: {
+        port: 3000,
         proxy: {
             '/api': {
                 target: 'http://localhost:5000',
