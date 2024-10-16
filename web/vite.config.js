474a0d4a8d936429bfc3d4bdab7192511567ada8
Christian Bick <christian.bick@bitsuppliers.com>
2024-10-16 15:20:42 +0200

    Fix Github action

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,14 +1,21 @@
 import {defineConfig} from "vite";
 
-export default defineConfig({
-    server: {
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://api:5000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
+function getBase(command) {
+    return command === 'build' ? '/edu-graph/' : '/';
+}
+
+export default defineConfig(({command}) => {
+    return {
+        base: getBase(command),
+        server: {
+            port: 3000,
+            proxy: {
+                '/api': {
+                    target: 'http://api:5000',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
             },
-        },
-    },
+        }
+    }
 })
