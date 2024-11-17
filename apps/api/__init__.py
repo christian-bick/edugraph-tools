9644ed0c99bfd61a63d7bb1e31254996ed409e9d
Christian Bick <christian.bick@bitsuppliers.com>
2024-11-17 18:16:22 +0100

    Update imports

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -1,9 +1,6 @@
-import os
-
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
-from google import generativeai as gemini
 
 app = Flask(__name__, static_folder=None)
 
