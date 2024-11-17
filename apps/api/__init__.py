6c133b99807277dc52b4ee06d088935d7ee9d717
Christian Bick <christian.bick@bitsuppliers.com>
2024-11-17 17:02:06 +0100

    Fix loading of environment variable

--- apps/api/__init__.py
+++ apps/api/__init__.py
@@ -1,10 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
+from google import generativeai as gemini
 
 app = Flask(__name__, static_folder=None)
 
 from api import routes
 
+
 def create_app():
+    load_dotenv()
+    gemini.configure(api_key=os.environ["API_KEY_GEMINI"])
     CORS(app)
     return app
--- apps/app.py
+++ apps/app.py
@@ -1,12 +1,5 @@
-import os
 
-from dotenv import load_dotenv
-from google import generativeai as gemini
 
 from api import create_app
-
-load_dotenv()
-
-gemini.configure(api_key=os.environ["API_KEY_GEMINI"])
 
 app = create_app()
